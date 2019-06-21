1ed64dca43266133fc4b55577410d51fe7a454e6
ETY001 <ety001@domyself.me>
2019-06-21 18:13:13 +0800

    auth_login finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 'use strict';
 const env = process.env;
 
-const Hapi = require('hapi');
+const Hapi = require('@hapi/hapi');
+const steem = require('steem');
+
+const getAccounts = function(accounts) {
+  return new Promise((resolve, reject) => {
+    steem.api.getAccounts(accounts, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
 
 const server = Hapi.server({
   port: 8899,
 });
 const api_url = env.api_url ? env.api_url : '';
+steem.api.setOptions({url: api_url});
 
 server.route({
   method: 'GET',
@@ -24,16 +38,47 @@ server.route({
 });
 
 server.route({
-  method: 'GET',
-  path: '/global_config',
+  method: 'POST',
+  path: '/auth_login',
   options: {
     cors: true
   },
   handler: (request, h) => {
-    return {status: true, result: {
-      api_url,
-    }};
-  }
+    const payload = request.payload;
+    const username = payload.username;
+    const posting = payload.posting;
+    if (!username || !posting) {
+      return {
+        status: false,
+        msg: 'params_error',
+      };
+    }
+    return getAccounts([username])
+      .then((res) => {
+        if (res === []) {
+          return {
+            status: false,
+            msg: 'login_failed',
+          };
+        }
+        const postingKeyAuths = res[0]['posting']['key_auths'];
+        const inputPostKeyAuth = steem.auth.wifToPublic(posting);
+        const result = postingKeyAuths.filter(p => p[0] === inputPostKeyAuth);
+        if (result.length > 0) {
+          return {
+            status: true,
+            msg: 'login_success',
+          };
+        }
+        return {
+          status: false,
+          msg: 'login_failed',
+        };
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 });
 
 const init = async () => {
